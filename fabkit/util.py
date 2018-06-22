4584e5969f6bbab9dfb00734746ccbbf07758e37
Karol Horowski <karol.hor@gmail.com>
2018-06-22 10:04:56 +0200

    fix failing run_or_sudo

--- fabkit/util.py
+++ fabkit/util.py
@@ -3,4 +3,4 @@ from fabric.operations import run, sudo
 
 def run_or_sudo(cmd, use_sudo, **kwargs):
     runner = sudo if use_sudo else run
-    return runner(cmd=cmd, **kwargs)
+    return runner(cmd, **kwargs)
