3a1610996f1c7136b57397696d04249bbaee731f
Karol Horowski <karol.hor@gmail.com>
2018-06-27 14:39:08 +0200

    manage files/users

--- fabkit/files.py
+++ fabkit/files.py
@@ -19,6 +19,15 @@ def exists(path, use_sudo: False):
     return _test('-e', path, use_sudo)
 
 
+def chown(path, user=None, group=None, use_sudo=False):
+    if not user and not group:
+        raise ValueError("[chown] One of user/group must be provided")
+    if group:
+        group = f':{group}'
+
+    run_or_sudo(f'chown {user}{group} {quote(path)}', use_sudo)
+
+
 def _test(option, path, use_sudo):
     with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
         return run_or_sudo(f'test {option} {quote(path)}', use_sudo).succeeded
